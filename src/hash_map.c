3485547306c828f4924e5b9abbd2ef0483bc3186
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-03-09 11:17:07 +0530

    add: hash fn and copy string

--- src/hash_map.c
+++ src/hash_map.c
@@ -1,8 +1,9 @@
 #include "./hash_map.h"
 
 #include <stddef.h> // NULL
+#include <stdint.h> // uint32_t, ...
 #include <stdlib.h> // mem alloc
-#include <string.h> // memset
+#include <string.h> // memset, strdup, strlen
 
 void initMap(Map* map){
     map->capacity = 0;
@@ -25,4 +26,24 @@ void growMap(Map* map, int capacity){
     map->entries = entries;
     map->capacity = capacity;
     map->count = 0;
+}
+
+static uint32_t hashString(const char *chars){
+    // FNV-1
+    uint32_t hash = 2166136261u;
+    while (*chars){
+        hash = (hash ^ (uint8_t)chars[0]*16777619);
+        chars++;
+    }
+
+    return hash;
+}
+
+String *copyString(const char *chars){
+    // heap cuz stack var can't outlive the fn
+    String *string = malloc(sizeof(String));
+    string->hash = hashString(chars);
+    string->len = strlen(chars);
+    string->chars = strdup(chars);
+    return string;
 }
