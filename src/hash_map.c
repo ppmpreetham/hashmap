233048c5aa0f1daa2b1579026c0968827e1c67bd
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-03-09 11:00:26 +0530

    add: growMap funtion

--- src/hash_map.c
+++ src/hash_map.c
@@ -1,5 +1,8 @@
 #include "./hash_map.h"
 
+#include <stdlib.h> // mem alloc
+#include <string.h> // memset
+
 void initMap(Map* map){
     map->capacity = 0;
     map->count = 0;
@@ -9,4 +12,13 @@ void initMap(Map* map){
 void freeMap(Map* map){
     free(map->entries);
     initMap(map);
+}
+
+void growMap(Map* map, int capacity){
+    Entry *entries = malloc(capacity*sizeof(Entry));
+    memset(entries, 0, capacity*sizeof(Entry));
+    free(map->entries);
+    map->entries = entries;
+    map->capacity = capacity;
+    map->count = 0;
 }
